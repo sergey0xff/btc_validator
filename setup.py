46518123af9f3ac4361acff13d7de395ed6e2389
Sergey Tikhonov <srg.tikhonov@gmail.com>
2020-01-10 12:40:41 +0300

    Added version to setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="btc_validator",
-    version="1.0.0",
+    version="1.0.1",
     description="Bitcoin address validator",
     long_description=README,
     long_description_content_type="text/markdown",
